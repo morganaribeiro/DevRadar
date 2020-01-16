00fcff5e4c84dd64a883d6befeb974058c48e191
LeoGhz <djleonardhd@hotmail.com>
2020-01-16 21:44:31 +0000

    Day #2

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 <img src="./static/omnistack.png" align="center"></img>
 <h1 align="center">Semana Omnistack 10</h1>
-<p align="center">Projeto <strong>DevRadar</strong>Desenvolvido durante a 10ª Semana Omnistack da Rocketseat</p>
+<p align="center">Projeto <strong>DevRadar</strong> Desenvolvido durante a 10ª Semana Omnistack da Rocketseat</p>
 
 <p align="center">
   <a aria-label="Versão do Node" href="https://github.com/nodejs/node/blob/master/doc/changelogs/CHANGELOG_V12.md#12.14.1">
     <img src="https://img.shields.io/badge/node.js@lts-12.14.1-informational?logo=Node.JS"></img>
-  </a>
+D</a>
   <a aria-label="Versão do React" href="https://github.com/facebook/react/blob/master/CHANGELOG.md#16120-november-14-2019">
     <img src="https://img.shields.io/badge/react-16.12.0-informational?logo=react"></img>
   </a>
   <a aria-label="Desafios" href="DESAFIOS.md">
-  	<img src="https://img.shields.io/badge/desafios-OK-blueviolet"></img>
+  	<img src="https://img.shields.io/badge/desafios-OK-green"></img>
   </a>
---
+++ frontend/src/components/DevForm/index.js
@@ -0,0 +1,111 @@
+import React, { useState, useEffect } from 'react';
+
+import './style.css';
+
+function DevForm({ onAdd, onEdit, editModeState }){
+    const [{editMode, dev}, setEditMode] = editModeState;  
+    const [github, setGithub] = useState('');
+    const [techs, setTechs] = useState('');
+    const [latitude, setLatitude] = useState('');
+    const [longitude, setLongitude] = useState('');
+
+    useEffect(() => {
+      if (!editMode) {
+        setGithub('');
+        setTechs('');
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const { latitude, longitude } = position.coords;
+    
+            setLatitude(latitude);
+            setLongitude(longitude);
+          },
+          (err) => {
+            console.log(err);
+          }, {
+            timeout: 30000
+          }
+        )
+      } else {
+        const { github, techs, location: { coordinates: [latitude, longitude]}} = dev;
+        setGithub(github);
+        setTechs(techs.join(", "));
+        setLatitude(latitude);
+        setLongitude(longitude);
+      }
+    }, [editMode, dev]);
+
+    async function handleSubmit(e){
+        e.preventDefault();
+        if (editMode) {
+          await onEdit(dev, {
+            techs,
+            latitude,
+            longitude
+          })
+          setEditMode({editMode: false, dev: {}});
+        } else 
+        await onAdd({
+            github,
+            techs,
+            latitude,
+            longitude
+        });
+        setGithub('');
+        setTechs('');
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+        <div className="input-block">
+          <label htmlFor="github">Usuário do GitHub</label>
+          <input 
+            name="github" 
+            id="github" 
+            required
+            value={github}
+            disabled={editMode}
+            onChange={e => setGithub(e.target.value)}
+          />
+        </div>
+    
+        <div className="input-block">
+          <label htmlFor="techs">Tecnologias</label>
+          <input 
+            name="techs" 
+            id="techs" 
+            required
+            value={techs}
+            onChange={e => setTechs(e.target.value)}
+          />
+        </div>
+    
+        <div className="input-group">
+          <div className="input-block">
+            <label htmlFor="latitude">Latitude</label>
+            <input 
+              name="latitude" 
+              id="latitude" 
+              value={latitude} 
+              required
+              onChange={e => setLatitude(e.target.value)}
+            />
+          </div>
+          <div className="input-block">
+            <label htmlFor="longitude">Longitude</label>
+            <input 
+              name="longitude"
+              id="longitude" 
+              value={longitude} 
+              required
+              onChange={e => setLongitude(e.target.value)}
+            />
+          </div>
+        </div>
+    
+        <button type="submit">Salvar</button>
+      </form>
+    );
+}
+
+export default DevForm;
